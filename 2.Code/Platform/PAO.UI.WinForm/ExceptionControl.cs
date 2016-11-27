fe6f84040240a7c8b5adb4441d8d26f9a237b29e
刘丹 <200724@QQ.COM>
2016-11-27 22:29:57 +0800

    增加了ExceptionControl

---
+++ 2.Code/Platform/PAO.UI.WinForm/ExceptionControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace PAO.UI.WinForm
+{
+    public partial class ExceptionControl : DevExpress.XtraEditors.XtraUserControl
+    {
+        public ExceptionControl() {
+            InitializeComponent();
+        }
+    }
+}
