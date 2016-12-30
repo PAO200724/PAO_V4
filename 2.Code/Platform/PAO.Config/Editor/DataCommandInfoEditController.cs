04a891a670bae63854ce4c028b2764aec0370d26
刘丹 <200724@QQ.COM>
2016-12-30 09:00:21 +0800

    增加了Editor的设计，增加了若干Controller

---
+++ 2.Code/Platform/PAO.Config/Editor/DataCommandInfoEditController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using PAO;
+using PAO.WinForm;
+
+namespace PAO.Config.Editor
+{
+    /// <summary>
+    /// 类：DataCommandInfoEditController
+    /// 数据命令编辑控制器
+    /// 数据命令编辑器的控制器
+    /// 作者：PAO
+    /// </summary>
+    [Addon]
+    [Serializable]
+    [DataContract(Namespace = "")]
+    [Name("数据命令编辑控制器")]
+    [Description("数据命令编辑器的控制器")]
+    public class DataCommandInfoEditController : BaseObjectEditController
+    {
+        #region 插件属性
+        #endregion
+        public DataCommandInfoEditController() {
+        }
+
+        protected override BaseEditControl OnCreateEditControl() {
+            var editControl = new DataCommandInfoEditControl();
+            return editControl;
+        }
+    }
+}
---
+++ 2.Code/Platform/PAO.Config/Editor/DataFilterEditController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using PAO;
+using PAO.WinForm;
+
+namespace PAO.Config.Editor
+{
+    /// <summary>
+    /// 类：DataFilterEditController
+    /// 数据过滤器编辑控制器
+    /// 数据过滤器编辑器的控制器
+    /// 作者：PAO
+    /// </summary>
+    [Addon]
+    [Serializable]
+    [DataContract(Namespace = "")]
+    [Name("数据过滤器编辑控制器")]
+    [Description("数据过滤器编辑器的控制器")]
+    public class DataFilterEditController : BaseObjectEditController
+    {
+        #region 插件属性
+        #endregion
+        public DataFilterEditController() {
+        }
+
+        protected override BaseEditControl OnCreateEditControl() {
+            var editControl = new DataFilterEditControl();
+            return editControl;
+        }
+    }
+}
